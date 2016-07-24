f82be56643e8d28e9e00e19982dbd582289ac8cf
Roberto Quintanilla <roberto.quintanilla@gmail.com>
2016-07-24 09:07:44 -0500

    Fixed: Product search scope with includes failed due to changes in ActiveRecord/Arel from 4.2.x to 5.0.x

--- app/models/concerns/sortable_by_given_scores.rb
+++ app/models/concerns/sortable_by_given_scores.rb
@@ -3,22 +3,31 @@ module SortableByGivenScores
 
   module ClassMethods
     def scores_as_recordset(scores, recordset_alias = :record_scoring)
-      Arel::Nodes::NamedFunction.new(
+      select_manager = Arel::SelectManager.new
+
+      select_manager.from Arel::Nodes::NamedFunction.new(
         "json_populate_recordset", [
         Arel::Nodes::SqlLiteral.new("NULL::record_score"),
         Arel::Nodes::SqlLiteral.new("'#{
           ActiveSupport::JSON.encode(scores)
         }'")
-      ]).as "\"#{recordset_alias}\""
+      ])
+
+      select_manager.project Arel.star
     end
 
     def by_given_scores(scores, recordset_alias = :record_scoring)
-      scoring = Arel::Table.new recordset_alias # Not a real table...
-      scoring_recordset = scores_as_recordset(scores, recordset_alias)
-      join_condition = scoring_recordset.create_on(scoring[:id].eq(arel_table[:id]))
+      # The generated recordset as a table-like sub-query:
+      scoring_recordset = scores_as_recordset(scores).as "\"#{recordset_alias}\""
+
+      # The join conditions to the generated recordset:
+      join_condition = scoring_recordset
+        .create_on(scoring_recordset[:id].eq(arel_table[:id]))
+
       join_clause = arel_table
         .create_join(scoring_recordset, join_condition, Arel::Nodes::InnerJoin)
-      joins(join_clause).order(scoring[:score].desc)
+
+      joins(join_clause).order(scoring_recordset[:score].desc)
     end
   end
 end
